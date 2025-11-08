1f1e228c58c5d488831ddc7363606980569b8b72
saifanidk <saifan7787790@gmail.com>
2025-11-08 14:31:55 +0100

    Update lineage_cancunf.mk

--- lineage_cancunf.mk
+++ lineage_cancunf.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/motorola/cancunf/device.mk)
 
-# Inherit some common YAAP stuff.
+# Inherit some common lineage stuff.
 $(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
